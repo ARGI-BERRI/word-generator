52afb60dbbc5a66c5c4d6e6f784c733651ad0c81
ARGI-BERRI <me@argia.dev>
2023-06-19 23:26:33 +0900

    feat: improved config experience
    Signed-off-by: ARGI-BERRI <me@argia.dev>

--- src/generate.py
+++ src/generate.py
@@ -13,14 +13,21 @@ def main():
         sounds[sound] = sounds[sound].split(' ')
 
     for pattern in patterns:
+        pattern.setdefault('label', 'no label')
+        pattern.setdefault('count', 5)
+
+        print(f'Pattern name: {pattern["label"]}')
+        print(f'Pattern: x{pattern["count"]} {pattern["syllable"]}')
+
         for _ in range(pattern['count']):
             words: list[str] = list()
 
             for sound in pattern['syllable']:
                 words.append(random.choice(sounds[sound]))
 
-            print(''.join(words))
-
+            print(''.join(words), end=', ')
+        
+        print('\n')
 
 if __name__ == '__main__':
     main()
